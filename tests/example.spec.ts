0856d35ee755e9c71a9d376610fb2e8e3fd1e939
Raul Garcia <raulgarciagranados96@gmail.com>
2023-02-20 22:16:43 -0600

    Adding tags and modify encrypted test case

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,20 +1,21 @@
-import { expect } from '@playwright/test';
 import test from "../src/utils/fixtures";
 import * as testData from './testData/testData.json';
 
 test('Create new password encrypted', async({stringUtils}) => {
-  const encrypt_pwd = stringUtils.encryptPassword(testData.LOGIN.APP_PASSWORD);
+  const encrypt_pwd = stringUtils.encryptPassword(testData.LOGIN.DB_PASSWORD);
   console.log(encrypt_pwd);
+  const decrypt_pwd = stringUtils.decipherAppPassword(testData.LOGIN.APP_PASSWORD);
+  console.log(decrypt_pwd);
 })
 
-test('Correctly login in App', async ({ page, loginSteps}) => {
+test('Correctly login in App @login', async ({ page, loginSteps}) => {
   await page.goto('https://www.saucedemo.com/');
   await loginSteps.enterUserName(testData.LOGIN.APP_USERNAME);
   await loginSteps.enterPassword(testData.LOGIN.APP_PASSWORD);
   await loginSteps.clickSubmitButton(); 
 });
 
-test('User Locked out in App', async ({ page, loginSteps }) => {
+test('User Locked out in App @login', async ({ page, loginSteps }) => {
   await page.goto('https://www.saucedemo.com/');
   await loginSteps.enterUserName(testData.LOGIN.APP_USERNAME_LOCKED_OUT);
   await loginSteps.enterPassword(testData.LOGIN.APP_PASSWORD);
